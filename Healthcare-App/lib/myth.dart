ff3175e23b4ebdf03b8b232a7c225b3d409d9c15
Rishabh Jain <jain.rishabhche@gmail.com>
2021-02-15 02:01:34 +0530

    added carousel_slider for the images

---
+++ Healthcare-App/lib/myth.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class MythPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Myths',
+          ),
+          centerTitle: true,
+          elevation: 0.0,
+        ),
+        body: CarouselSlider(
+          options: CarouselOptions(
+            height: 1000,
+            autoPlay: true,
+            autoPlayInterval: Duration(seconds: 40),
+            autoPlayAnimationDuration: Duration(milliseconds: 800),
+            autoPlayCurve: Curves.fastOutSlowIn,
+            enlargeCenterPage: true,
+            // onPageChanged: callbackFunction,
+            scrollDirection: Axis.horizontal,
+          ),
+          items: [1, 2, 3, 4, 5, 6, 7].map((i) {
+            return Builder(
+              builder: (BuildContext context) {
+                return Container(
+                  height: 100,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image(
+                    fit: BoxFit.fitWidth,
+                    image: AssetImage(
+                      "Images/$i.png",
+                    ),
+                  ),
+                );
+              },
+            );
+          }).toList(),
+        ));
+  }
+}
